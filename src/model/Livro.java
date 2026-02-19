a118f713aa3fb9ae038732c7aa1de8e3985181bd
camille <camilledds0@gmail.com>
2026-02-19 16:02:20 -0300

    Initial commit - LibCore

---
+++ src/model/Livro.java
@@ -0,0 +1,13 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(String titulo, boolean disponivel, String autor) {
+        this.titulo = titulo;
+        this.disponivel = disponivel;
+        this.autor = autor;
+    }
+    
+
+}
